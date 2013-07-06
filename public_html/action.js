a54950b63bb147a5162a875e03a0dbdf30a593e8
Kevin Xu <kevinx@kevinx-macbookpro2.home>
2013-07-06 12:58:56 -0700

    some site cleanup

--- public_html/action.js
+++ public_html/action.js
@@ -2,9 +2,9 @@
    // do stuff when DOM is ready
 	$(".megaten-infopage").hide();
 	$("#top").show();
-	$(".megaten-links > li > a").click(function(event) {
-     var divName = $(this).attr('href').substring(1);
-	 $(".megaten-infopage").hide();
-	 $("#" + divName).show();
-   });
+	$("table.megaten-links td").click(function(event) {
+	   var divName = $(this).find('a').attr('href').substring(1);
+	   $(".megaten-infopage").hide();
+	   $("#" + divName).show();
+	});
  });
